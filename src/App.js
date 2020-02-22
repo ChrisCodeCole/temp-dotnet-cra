e5ad0912a70629f430f2e627b66cf6defdbfe566
Helen <hchang0803@outlook.com>
2020-02-22 11:44:38 -0500

    add social icon, footer, and madebyengineerforengineers components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Route} from 'react-router';
-import {Layout} from './components/Layout';
-import {Home} from './components/Home';
-import {FetchData} from './components/FetchData';
-import {Counter} from './components/Counter';
-
+import { Route } from 'react-router';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+import { FetchData } from './components/FetchData';
+import { Counter } from './components/Counter';
+import Helen from './components/Helen';
 import './custom.css';
 
 const App = () => {
@@ -15,6 +15,7 @@ const App = () => {
 			<Route exact path='/' component={Home} />
 			<Route path='/counter' component={Counter} />
 			<Route path='/fetch-data' component={FetchData} />
+			<Route path='/helen' component={Helen} />
 		</Layout>
 	);
 };
---
+++ src/components/Helen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SocialIcons from './SocialIcons/SocialIcons';
+import MadeByEngineersForEngineers from './MadeByEngineersForEngineers/MadeByEngineersForEngineers';
+import Footer from './Footer/Footer';
+
+const Helen = () => {
+	return (
+		<div>
+			<MadeByEngineersForEngineers />
+			<SocialIcons />
+			<Footer />
+		</div>
+	);
+};
+
+export default Helen;
