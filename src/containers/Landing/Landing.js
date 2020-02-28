d7df37e38999def46ad54896d724d370830d28a7
Stanley Chen <dannyshien@gmail.com>
2020-02-28 18:47:53 -0500

    NPM install and minor edits to discription css

--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react'
-import classes from './Landing.module.css'
+import React, { Component } from 'react';
+import classes from './Landing.module.css';
+
+import Description from './../../components/Description/Description';
+// import Image from './../../components/Image/Image';
+// import Button from './../../components/UI/button/Buttons';
 
 export default class Landing extends Component {
-    render() {
-        return (
-            <div className={classes.landingWrapper}>
-                Landing
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className={classes.landingWrapper}>
+				Landing
+				<Description />
+			</div>
+		);
+	}
 }
