29199d4cb4ccece94efe356ed38cce81eb169226
Stanley Chen <dannyshien@gmail.com>
2020-02-28 15:26:20 -0500

    add new button component and correct typo for descritption.module.css

---
+++ src/components/UI/button/Buttons.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import classes from './Button.module.css';
+
+const Button = () => {
+	return (
+		<Link classname={classes.link} to='#'>
+			Button
+		</Link>
+	);
+};
+
+export default Button;
