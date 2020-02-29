d96faf9c0cdc9742f8af8402f23c44d3a72710e6
Stanley Chen <dannyshien@gmail.com>
2020-02-28 22:37:34 -0500

    create a button component, will revise for functionality

--- src/components/UI/button/Buttons.js
+++ src/components/UI/button/Buttons.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import classes from './Button.module.css';
 
+//
 const Button = () => {
 	return (
 		<Link classname={classes.link} to='#'>
