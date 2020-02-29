a627144a158e13d901fa677c1a3a0f47a1e11d61
Helen <hchang0803@outlook.com>
2020-02-29 08:58:40 -0500

    add contact, profile, and team containers

---
+++ src/containers/Profile/Profile.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+const Profile = () => (
+	<Fragment>
+		<h1>Profile</h1>
+	</Fragment>
+);
+
+export default Profile;
---
+++ src/containers/Team/Team.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+const Team = () => (
+	<Fragment>
+		<h1>Team</h1>
+	</Fragment>
+);
+
+export default Team;
