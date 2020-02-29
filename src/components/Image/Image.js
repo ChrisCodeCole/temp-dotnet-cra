701e0a6bc49f0d2165aab07ac4bfbb2bd8636262
Stanley Chen <dannyshien@gmail.com>
2020-02-28 22:23:39 -0500

    Add relative path to test image component and also add minor css for image stylig

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import classes from './Image.module.css';
+import IMG from './../../assets/a1.png';
 
 const Image = () => {
 	return (
 		<div className={classes.imageContainer}>
-			<img className={classes.image} src='#' alt='' />
+			<img className={classes.image} src={IMG} alt='' />
 		</div>
 	);
 };
--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -1,17 +1,8 @@
 import React, { Component } from 'react';
 import classes from './Landing.module.css';
 
-import Description from './../../components/Description/Description';
-// import Image from './../../components/Image/Image';
-// import Button from './../../components/UI/button/Buttons';
-
 export default class Landing extends Component {
 	render() {
-		return (
-			<div className={classes.landingWrapper}>
-				Landing
-				<Description />
-			</div>
-		);
+		return <div className={classes.landingWrapper}>Landing</div>;
 	}
 }
