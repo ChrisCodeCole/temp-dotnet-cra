718a98b42ca88b7513509e42c103c6a300393fa1
Stanley Chen <dannyshien@gmail.com>
2020-03-03 16:31:10 -0500

    setting up about1

--- src/containers/About1/About1.js
+++ src/containers/About1/About1.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+import Navbar from '../../components/Navbar';
+import Header from '../../components/Header';
+// import Image from '../../components/'
+
 class About1 extends Component {
 	render() {
 		return <div>About 1</div>;
