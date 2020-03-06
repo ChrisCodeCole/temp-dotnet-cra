fdca202cbc9abdda1b5d15840062342f7369d933
Stanley Chen <dannyshien@gmail.com>
2020-03-06 15:29:03 -0500

    complete most of grid for first about2Content section

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import classes from './Description.module.css';
 
-const Description = ({ style }) => {
+const Description = ({ style, text }) => {
 	return (
 		<div className={classes.descriptionContainer} style={style}>
-			<p className={classes.descriptionText}>
-				Aliqua commodo sint cillum enim ipsum sit non esse velit amet.
-				Cupidatat officia minim minim nisi et cillum aliqua aute. Non
-				pariatur qui veniam ad cupidatat quis qui occaecat dolor. Velit
-				tempor ea cupidatat nulla velit mollit est aliquip quis proident
-				in. Proident ad cupidatat reprehenderit commodo officia esse
-				culpa enim ea voluptate tempor ut deserunt. Anim minim irure
-				fugiat pariatur culpa excepteur enim eu culpa nulla elit
-				consectetur nulla Lorem. Tempor ipsum mollit velit Lorem anim
-				ullamco officia velit enim voluptate non.
-			</p>
+			<p className={classes.descriptionContent}>{text}</p>
 		</div>
 	);
 };
--- src/containers/About2/About2.js
+++ src/containers/About2/About2.js
@@ -4,6 +4,7 @@ import Navbar from '../../components/Navbar/NavBar';
 import Header from '../../components/Header/Header';
 import Image from '../../components/Image/Image';
 import SubHeader from '../../components/SubHeader/SubHeader';
+import Description from '../../components/Description/Description';
 import classes from './About2.module.css';
 
 class About2 extends Component {
@@ -12,29 +13,61 @@ class About2 extends Component {
 			<div className={classes.about2Container}>
 				<Navbar />
 				<div className={classes.about2Content}>
-					<Header headerText='01' />
-					<Image />
-					<SubHeader
-						subheaderText='Name'
-						descriptionText='In veniam ad irure laboris tempor voluptate tempor enim tempor. Aliquip velit ipsum sunt eu ut tempor ex deserunt sint. Mollit do ipsum dolor est minim consectetur qui proident. Ea aliqua nulla quis incididunt irure. Culpa mollit aliqua ullamco reprehenderit enim quis ad mollit do est mollit dolor.'
-					/>
+					<div
+						className={classes.headerImage}
+						style={{ gridArea: 'headerImage' }}
+					>
+						<Header
+							headerText='01'
+							style={{
+								gridArea: 'header',
+							}}
+							// style={{
+							//     display: 'flex',
+							//     justifyContent: 'flex-start'
+							// }}
+						/>
+						<Image style={{ gridArea: 'image' }} />
+					</div>
+					<div
+						className={classes.textInfo}
+						style={{ gridArea: 'textInfo' }}
+					>
+						<Description text='Name' style={{ gridArea: 'name' }} />
+						<Description
+							text='Ea irure laboris sint exercitation cillum. Aute labore minim labore dolore incididunt. Velit mollit veniam adipisicing aliqua. Exercitation adipisicing esse id incididunt ad aliquip et ullamco deserunt. Voluptate enim nisi esse esse veniam fugiat occaecat ex in quis id aliquip. Ut esse irure id proident proident laboris pariatur anim nisi voluptate culpa pariatur commodo aute.'
+							style={{ gridArea: 'text' }}
+						/>
+					</div>
 				</div>
-				<div className={classes.about2Content}>
+
+				{/* <div className={classes.about2Content}>
 					<Header headerText='02' />
 					<Image />
 					<SubHeader
 						subheaderText='Name'
 						descriptionText='Eiusmod ut magna ipsum labore consequat esse non occaecat mollit. Veniam incididunt et pariatur non pariatur eu consequat aliquip sint. Laboris esse ullamco do esse. Ullamco cupidatat est amet officia quis quis ex.'
 					/>
-				</div>
-				<div className={classes.about2Content}>
-					<Header headerText='03' />
-					<Image />
-					<SubHeader
-						subheaderText='Name'
-						descriptionText='Do ex et excepteur aute ipsum ad sit magna amet. Cupidatat ad eu et et nulla ea minim enim. Non ex eiusmod minim ipsum ea.'
-					/>
-				</div>
+                </div> */}
+
+				{/* <div className={classes.about2Content}>
+                    <div className={classes.headerImage}>
+					    <Header headerText='01' style={{}}/>
+					    <Image />
+                    </div>
+                    <div className={classes.subHeader}>
+					    <SubHeader
+                            subheaderText='Name'
+                            descriptionText='In veniam ad irure laboris tempor voluptate tempor enim tempor. Aliquip velit ipsum sunt eu ut tempor ex deserunt sint. Mollit do ipsum dolor est minim consectetur qui proident. Ea aliqua nulla quis incididunt irure. Culpa mollit aliqua ullamco reprehenderit enim quis ad mollit do est mollit dolor.'
+                            style={{
+                                display: 'flex', 
+                                flexDirection: 'column',
+                                justifyContent: 'center',
+                                height: '100%'
+                            }}
+                        />
+                    </div>
+				</div> */}
 			</div>
 		);
 	}
