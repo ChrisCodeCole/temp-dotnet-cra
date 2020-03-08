7e04ecc9181bc49adf9a20bd43bf1381725ef763
Christian Coleman <christian.c2508@gmail.com>
2020-03-08 10:39:38 -0400

    added landing | updated some routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,7 +4,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from '../containers/Home/Home';
 import Landing from '../containers/Landing/Landing';
 import Login from '../containers/Login/Login';
-import About2 from '../containers/About2/About2';
+import Connections from '../containers/Connections/Connections';
 
 const Team = React.lazy(() => {
 	return import('../containers/Team/Team');
@@ -23,16 +23,16 @@ const Profile = React.lazy(() => {
 const routes = () => (
 	<Switch>
 		<Route path='/login' component={Login} />
-		{/* <Route path='/home' component={Home} /> */}
-		{/* <Route exact path='/team' component={Team} /> */}
-		{/* <Route exact path='/contact' component={Contact} /> */}
-		{/* <Route exact path='/profile' component={Profile} /> */}
+		<Route path='/home' component={Home} />
+		<Route exact path='/team' component={Team} />
+		<Route exact path='/contact' component={Contact} />
+		<Route exact path='/profile' component={Profile} />
 		{/* <Route path='/helen' component={Helen} /> */}
-		{/* <Route exact path='/' component={Landing} /> */}
-		<Route exact path='/' component={About2} />
+		<Route exact path='/' component={Landing} />
+		<Route exact path='/connections' component={Connections} />
 
 		{/* Since we do not have a 404 page right now - this solution works */}
-		{/* <Redirect to='/' /> */}
+		<Redirect to='/' />
 	</Switch>
 );
 
